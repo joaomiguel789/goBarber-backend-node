4989a91e9d0813c1c574764859bdf949bb1a1e28
Miguel <joao.martins789@gmail.com>
2020-08-29 11:07:04 -0300

    configurando estrutura

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (resquest, response) => {
+    return response.json({ message: 'Hello GoStack'})
+})
+
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333')    
+})
